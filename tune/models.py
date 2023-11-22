661f5bad217ff34995d44de5a6ab54557db1afd9
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-22 10:54:07 -0500

    Update some model fields including choices for meters

--- tune/models.py
+++ tune/models.py
@@ -4,14 +4,24 @@ class Tune(models.Model):
     """
     The Tune is the heart of this app; each tune is one song that can be added to a user's repertoire and should contain all relevant musical information.
     """
+    METERS = [
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (9, '9'),
+        (10, 'irregular')
+    ]
+
     title = models.CharField(max_length=100, unique=True)
     composer = models.CharField(max_length=30, blank=True)
-    key = models.CharField(max_length=10, blank=True)           # The main key of a tune
-    other_keys = models.CharField(max_length=20, blank=True)    # Key(s) a tune modulates to in addition to the main key
+    key = models.CharField(max_length=10, blank=True, help_text='The main key of the tune')
+    other_keys = models.CharField(max_length=20, blank=True, help_text='Key(s) a tune modulates to in addition to the main key')
     song_form = models.CharField(max_length=20, blank=True)
     style = models.CharField(max_length=20, blank=True)
-    meter = models.CharField(max_length=10, blank=True)
-    year = models.CharField(max_length=10, blank=True)
+    meter = models.PositiveSmallIntegerField(choices=METERS, blank=True)
+    year = models.PositiveSmallIntegerField(blank=True)
     # players = models.ForeignKey(Player, related_name='tunes', on_delete=models.CASCADE) # This field defines which players (users) have the tune in their repertoire
     
     @property
