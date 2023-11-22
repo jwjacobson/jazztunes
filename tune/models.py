2152116f37e57b9b226b2ea67ad65ab661bda53a
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-22 11:09:45 -0500

    Add default values for certain fields

--- tune/models.py
+++ tune/models.py
@@ -27,13 +27,13 @@ class Tune(models.Model):
         ('irregular', 'irregular'),
     ]
 
-    title = models.CharField(max_length=100, unique=True)
-    composer = models.CharField(max_length=30, blank=True)
+    title = models.CharField(max_length=100, unique=True, help_text='The only required field')
+    composer = models.CharField(max_length=30, blank=True, help_text='Last names only for now')
     key = models.CharField(max_length=10, blank=True, help_text='The main key of the tune')
     other_keys = models.CharField(max_length=20, blank=True, help_text='Key(s) a tune modulates to in addition to the main key')
     song_form = models.CharField(choices=FORMS, max_length=15, blank=True)
-    style = models.CharField(choices=STYLES, max_length=15, blank=True)
-    meter = models.PositiveSmallIntegerField(choices=METERS, blank=True)
+    style = models.CharField(choices=STYLES, max_length=15, blank=True, default='standard')
+    meter = models.PositiveSmallIntegerField(choices=METERS, blank=True, default=4)
     year = models.PositiveSmallIntegerField(blank=True)
     # players = models.ForeignKey(Player, related_name='tunes', on_delete=models.CASCADE) # This field defines which players (users) have the tune in their repertoire
     
@@ -42,11 +42,11 @@ class Tune(models.Model):
         """
         Calculate the decade of composition based on the year of composition.
         """
-        decade = f'{self.year[2]}0s' 
+        decade = f'{str(self.year)[2]}0s' 
         return decade
 
     class Meta:
-        ordering = ['-id']
+        ordering = ['id']
 
     def __str__(self):
         return f'Tune {self.id} | {self.title}'
