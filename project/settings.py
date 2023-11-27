be5e78cdfeab2171390515e142161be22b99c7dd
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-27 13:38:33 -0500

    Integrate allauth login/signup/etc. into app

--- project/settings.py
+++ project/settings.py
@@ -53,6 +53,9 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+LOGIN_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
