50557f6432994ac044f6d029604db489f9e86411
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-01-26 09:55:30 -0500

    Update requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", "abcde")
 DEBUG = config("DEBUG", default=False, cast=bool)
 ADMIN_USER_ID = config("ADMIN_USER_ID", default=2, cast=int)
 
