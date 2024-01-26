51ec30332d70fc7e69a25f979603796589610099
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-01-26 10:56:14 -0500

    Update database info in settings.py

--- project/settings.py
+++ project/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = "project.wsgi.application"
 if "RDS_HOSTNAME" in os.environ:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.mysql",
+            "ENGINE": "django.db.backends.postgresql",
             "NAME": os.environ["RDS_DB_NAME"],
             "USER": os.environ["RDS_USERNAME"],
             "PASSWORD": os.environ["RDS_PASSWORD"],
