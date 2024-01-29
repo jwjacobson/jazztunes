376f818b89d63ff0efa680b9b1986aedd3f8e17c
Bob Belderbos <bobbelderbos@gmail.com>
2024-01-29 15:35:44 +0100

    fix url

--- project/settings.py
+++ project/settings.py
@@ -56,7 +56,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 LOGIN_URL = "account_login"
 LOGOUT_URL = "account_logout"
-LOGIN_REDIRECT_URL = "tune_list"
+LOGIN_REDIRECT_URL = "tune:tune_list"
 
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
