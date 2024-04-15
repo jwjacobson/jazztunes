6597a867b5eb8410f93d8e654ae3d2ea560ebfd8
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-04-15 15:02:24 -0400

    Add license notification text to core files

--- tune/forms.py
+++ tune/forms.py
@@ -1,3 +1,20 @@
+# jazztunes -- A jazz repertoire management app
+# Copyright (C) 2024 Jeff Jacobson <jeffjacobsonhimself@gmail.com>
+#
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from django.forms import ModelForm
 from django import forms
 from .models import Tune, RepertoireTune
