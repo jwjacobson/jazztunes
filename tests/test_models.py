91ea80b2f7881c386acc53a4de4a4045cce351cd
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-04-16 14:53:41 -0400

    Add reptune_tune_field_acess test

--- tests/test_models.py
+++ tests/test_models.py
@@ -77,3 +77,17 @@ def test_reptune_field_access(basic_reptune):
     assert rep_tune.player == user
     assert rep_tune.knowledge == "know"
     assert rep_tune.last_played == date(2024, 2, 1)
+
+
+@pytest.mark.django_db
+def test_reptune_tune_field_access(basic_reptune):
+    rep_tune, tune, user = basic_reptune
+
+    assert rep_tune.tune.title == "test title"
+    assert rep_tune.tune.composer == "test composer"
+    assert rep_tune.tune.key == "C"
+    assert rep_tune.tune.other_keys == "D Eb F#"
+    assert rep_tune.tune.song_form == "aaba"
+    assert rep_tune.tune.style == "standard"
+    assert rep_tune.tune.meter == 4
+    assert rep_tune.tune.year == 2023
