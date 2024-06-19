1b969d40b63eb64d1ec51a9836029f345469e631
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-06-19 11:35:35 -0400

    Change Sphinx theme to nature

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "classic"
+html_theme = "nature"
 html_static_path = ["_static"]
 
 
