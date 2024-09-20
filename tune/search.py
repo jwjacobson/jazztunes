c51d802191d269ed6a2b8345bf4d3c4e8577a636
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-09-20 15:03:13 -0400

    Remove unnecessary nickname_query declaration from query_tunes

--- tune/search.py
+++ tune/search.py
@@ -49,12 +49,18 @@ def exclude_term(tune_set, search_term):
     return term_query
 
 
+def nickname_search(tune_set, search_term):
+    nickname_query = tune_set.filter(
+        Q(tune__composer__icontains=Tune.NICKNAMES[search_term])
+    )
+    return nickname_query
+
+
 def query_tunes(tune_set, search_terms, timespan=None):
     """
-    Runs a search of the user's repertoire and return the results.
+    Run a search of the user's repertoire and return the results.
     """
     searches = set()
-    nickname_query = None
 
     for term in search_terms:
         if term and term[0] == "-":
@@ -82,10 +88,7 @@ def query_tunes(tune_set, search_terms, timespan=None):
             )
 
             if term in Tune.NICKNAMES:
-                nickname_query = tune_set.filter(
-                    Q(tune__composer__icontains=Tune.NICKNAMES[term])
-                )
-                term_query = term_query | nickname_query
+                term_query |= nickname_search(tune_set, term)
 
         if timespan is not None:
             term_query = term_query.exclude(last_played__gte=timespan)
@@ -102,7 +105,7 @@ def query_tunes(tune_set, search_terms, timespan=None):
 
 def return_search_results(request, search_terms, tunes, search_form, timespan=None):
     """
-    Function that runs query_tunes and returns the tune set to the view that called it.
+    Run query_tunes and return the results to the view that called it.
     """
     if len(search_terms) > Tune.MAX_SEARCH_TERMS:
         messages.error(
