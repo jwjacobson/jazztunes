c43be3c2116c1362e018e3bd84bff148aceb6ae4
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-02-12 15:17:32 -0500

    Update docs styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "jazztunes"
-copyright = "2024, Jeff Jacobson"
+copyright = "2024–2025, Jeff Jacobson"
 author = "Jeff Jacobson"
 
 # -- General configuration ---------------------------------------------------
@@ -28,9 +28,22 @@ html_theme = "furo"
 html_static_path = ["_static"]
 html_favicon = "_static/favicon.ico"
 
-html_sidebars = {
-    "**": [
-        "globaltoc.html",  # Table of contents
-        "searchbox.html",  # Search box
-    ],
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#A855F7",  # Pastel purple
+        "color-brand-content": "#0E7490",  # Turquoise
+        "color-admonition-background": "#FBCFE8",  # Lavender Blush
+        "color-background-primary": "#F9FAFB",  # Soft White
+        "color-background-secondary": "#E5E7EB",  # Warm Gray
+        "color-foreground-primary": "#374151",  # Muted Charcoal
+        "color-foreground-secondary": "#525252",  # Dark Gray
+        "color-inline-code": "#BFDBFE",  # Powder Blue
+        "color-link": "#0E7490",  # Turquoise
+        "color-link--hover": "#0EA5E9",  # Brighter turquoise
+        "color-sidebar-background": "#E6D6F2",  # Light pastel purple
+        "color-sidebar-link": "#6B21A8",  # Darker purple
+        "color-sidebar-link--hover": "#A855F7",  # Brighter purple
+        "color-sidebar-heading": "#4C1D95",  # Darker purple
+    },
 }
