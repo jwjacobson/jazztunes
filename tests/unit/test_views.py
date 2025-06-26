fd3050ac96760cd429aaa1d2b70c6fee0082adf5
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-06-26 12:51:18 -0400

    Move user_tune_rep and admin_tune_rep fixtures to unit/confest.py

--- tests/unit/test_views.py
+++ tests/unit/test_views.py
@@ -4,74 +4,10 @@ import pytest
 from datetime import datetime, timezone
 
 from django.urls import reverse
-from django.contrib.auth import get_user_model
 from django.contrib.messages import get_messages
-from django.conf import settings
 
 from tune.models import Tune, RepertoireTune
 from tune.forms import SearchForm
-
-
-@pytest.fixture
-def user_tune_rep(client):
-    """
-    Create a user, tune, and associated repertoire tune for use by views that require a single tune
-    """
-    user_model = get_user_model()
-    user = user_model.objects.create_user(username="testuser", password="12345")
-    client.force_login(user)
-
-    tune = Tune.objects.create(
-        title="test title",
-        composer="test composer",
-        key="C",
-        other_keys="D Eb F#",
-        song_form="aaba",
-        style="standard",
-        meter=4,
-        year=2023,
-    )
-
-    rep_tune = RepertoireTune.objects.create(
-        tune=tune,
-        player=user,
-        knowledge="know",
-        last_played=datetime(2024, 2, 1, 0, 0, 0, tzinfo=timezone.utc),
-    )
-
-    return {"tune": tune, "rep_tune": rep_tune, "user": user}
-
-
-@pytest.fixture
-def admin_tune_rep(client):
-    """
-    Create an admin user, tune, and associated repertoire tune for use by tune_browse
-    """
-    user_model = get_user_model()
-    admin = user_model.objects.create_user(username="admin", password="secret")
-    client.force_login(admin)
-
-    settings.ADMIN_USER_ID = admin.id
-
-    tune = Tune.objects.create(
-        title="test title",
-        composer="test composer",
-        key="C",
-        other_keys="D Eb F#",
-        song_form="aaba",
-        style="standard",
-        meter=4,
-        year=2023,
-    )
-
-    rep_tune = RepertoireTune.objects.create(
-        tune=tune,
-        player=admin,
-        knowledge="know",
-        last_played=datetime(2024, 2, 1, 0, 0, 0, tzinfo=timezone.utc),
-    )
-
-    return {"tune": tune, "rep_tune": rep_tune, "admin": admin}
 
 
 @pytest.mark.django_db
