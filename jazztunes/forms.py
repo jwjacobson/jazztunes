43d9ad19c79ea30a6f2e12600e360b4bb199eed1
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2026-02-06 12:40:48 -0500

    Add ignored jazztunes directory to verstion control

---
+++ jazztunes/forms.py
@@ -0,0 +1,195 @@
+# jazztunes -- A jazz repertoire management app
+# Copyright (C) 2024 Jeff Jacobson <jeffjacobsonhimself@gmail.com>
+#
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from datetime import timedelta
+
+from django import forms
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+from django.utils import timezone
+from django.utils.translation import gettext as _
+
+from .models import Tune, RepertoireTune
+
+
+class BaseForm:
+    """Base form mixin that applies consistent Tailwind styling to all form fields."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        input_classes = "w-full px-3 py-2 bg-orange-50 border border-black focus:outline-none focus:ring focus:ring-indigo-400"
+        select_classes = "w-full px-3 py-2.5 bg-orange-50 border border-black focus:outline-none focus:ring focus:ring-indigo-400"
+
+        for field_name, field in self.fields.items():
+            if field_name in getattr(self, "exclude_styling", []):
+                continue
+
+            if isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs["class"] = "mx-2"
+                continue
+
+            if (
+                hasattr(field.widget, "choices")
+                or "Select" in field.widget.__class__.__name__
+            ):
+                field.widget.attrs["class"] = select_classes
+            else:
+                field.widget.attrs["class"] = input_classes
+
+
+class TuneForm(BaseForm, ModelForm):
+    class Meta:
+        model = Tune
+        fields = [
+            "title",
+            "composer",
+            "key",
+            "other_keys",
+            "song_form",
+            "style",
+            "meter",
+            "year",
+        ]
+
+    def clean_key(self):
+        """
+        Check if the key entered is a real key and raise a ValidationError if it is not;
+        Properly format the key (title case)
+        """
+        data = self.cleaned_data["key"]
+        if data and data.lower() not in Tune.KEYS:
+            raise ValidationError(
+                'Invalid key (all normal keys accepted plus "none" and "atonal").'
+            )
+        return data.title()
+
+    def clean_other_keys(self):
+        """
+        Check if the other keys entered are real keys and raise ValidationErrors if they are not;
+        Properly format the keys (title case)
+        """
+        data = self.cleaned_data["other_keys"]
+        if data is None:
+            return data
+        formatted_data = []
+        for other_key in data.split():
+            if other_key.lower() not in Tune.KEYS:
+                raise ValidationError(_(f'"{other_key}" is not a valid key.'))
+            formatted_data.append(other_key.title())
+        data = " ".join(formatted_data)
+        return data
+
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class RepertoireTuneForm(BaseForm, ModelForm):
+    exclude_styling = ["tags"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if "tags" in self.fields:
+            self.fields["tags"].widget.attrs.update({"class": "accent-orange-100"})
+
+    class Meta:
+        model = RepertoireTune
+        exclude = ["tune", "player", "started_learning", "play_count"]
+        widgets = {"last_played": DateInput(), "tags": forms.CheckboxSelectMultiple()}
+
+    def save(self, commit=True):
+        instance = super(RepertoireTuneForm, self).save(commit=False)
+        if commit:
+            instance.save()
+            self.save_m2m()
+        return instance
+
+
+class TakeForm(RepertoireTuneForm):
+    """The Take form has custom coloration because it appears within a table cell that will have an orange background."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            current_classes = field.widget.attrs.get("class", "")
+            if field_name == "tags":
+                new_classes = current_classes.replace(
+                    "accent-orange-100", "accent-indigo-100"
+                )
+                field.widget.attrs["class"] = new_classes
+            else:
+                new_classes = current_classes.replace("bg-orange-50", "bg-indigo-50")
+                field.widget.attrs["class"] = new_classes
+
+
+class SearchForm(BaseForm, forms.Form):
+    TIMES = [
+        ("anytime", "n/a"),
+        ("day", "a day"),
+        ("week", "a week"),
+        ("month", "a month"),
+        ("two_months", "2 months"),
+        ("three_months", "3 months"),
+    ]
+
+    search_term = forms.CharField(label="search_term", max_length=200, required=False)
+    timespan = forms.ChoiceField(choices=TIMES, required=False)
+
+    def clean_timespan(self):
+        timespan = self.cleaned_data["timespan"]
+        if timespan == "day":
+            return timezone.now() - timedelta(days=1)
+        elif timespan == "week":
+            return timezone.now() - timedelta(days=7)
+        elif timespan == "month":
+            return timezone.now() - timedelta(days=30)
+        elif timespan == "two_months":
+            return timezone.now() - timedelta(days=60)
+        elif timespan == "three_months":
+            return timezone.now() - timedelta(days=90)
+        else:
+            return None
+
+
+class PlaySearchForm(SearchForm):
+    NORMAL_KEYS = [
+        "C",
+        "F",
+        "Bb",
+        "Eb",
+        "Ab",
+        "Db",
+        "Gb",
+        "B",
+        "E",
+        "A",
+        "D",
+        "G",
+    ]
+
+    ENHARMONICS = {
+        "C#": "Db",
+        "D#": "Eb",
+        "F#": "Gb",
+        "G#": "Ab",
+        "A#": "Bb",
+    }
+
+    suggest_key = forms.BooleanField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
---
+++ jazztunes/models.py
@@ -0,0 +1,191 @@
+# jazztunes -- A jazz repertoire management app
+# Copyright (C) 2024 Jeff Jacobson <jeffjacobsonhimself@gmail.com>
+#
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Tune(models.Model):
+    """
+    The Tune is the heart of this app; each tune is one song that can be added to a user's repertoire and should contain all relevant musical information.
+    """
+
+    METERS = [(3, "3"), (4, "4"), (5, "5"), (6, "6"), (7, "7"), (0, "irregular")]
+
+    STYLES = [("standard", "standard"), ("jazz", "jazz")]
+
+    FORMS = [
+        ("AABA", "AABA"),
+        ("ABAC", "ABAC"),
+        ("ABA", "ABA"),
+        ("ABAB", "ABAB"),
+        ("ABCD", "ABCD"),
+        ("AB", "AB"),
+        ("AAB", "AAB"),
+        ("AABC", "AABC"),
+        ("blues", "blues"),
+        ("irregular", "irregular"),
+    ]
+
+    KEYS = {
+        "c",
+        "f",
+        "bb",
+        "eb",
+        "ab",
+        "db",
+        "gb",
+        "b",
+        "e",
+        "a",
+        "d",
+        "g",
+        "a#",
+        "d#",
+        "g#",
+        "c#",
+        "f#",
+        "c-",
+        "f-",
+        "bb-",
+        "eb-",
+        "ab-",
+        "db-",
+        "gb-",
+        "b-",
+        "e-",
+        "a-",
+        "d-",
+        "g-",
+        "a#-",
+        "d#-",
+        "g#-",
+        "c#-",
+        "f#-",
+        "none",
+        "atonal",
+    }
+
+    MAX_SEARCH_TERMS = 10
+
+    NICKNAMES = {
+        "bird": "Parker",
+        "bud": "Powell",
+        "miles": "Davis",
+        "wayne": "Shorter",
+        "joe": "Henderson",
+        "lee": "Konitz",
+        "diz": "Gillespie",
+        "dizzy": "Gillespie",
+        "duke": "Ellington",
+        "sonny": "Rollins",
+        "bill": "Evans",
+        "herbie": "Hancock",
+        "cedar": "Walton",
+        "ornette": "Coleman",
+    }
+
+    field_names = {
+        "title",
+        "composer",
+        "key",
+        "keys",
+        "form",
+        "style",
+        "meter",
+        "year",
+        "tags",
+    }
+
+    title = models.CharField(max_length=90, help_text="The only required field")
+    composer = models.CharField(max_length=30, blank=True)
+    key = models.CharField(
+        max_length=10,
+        blank=True,
+    )
+    other_keys = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+    song_form = models.CharField(choices=FORMS, max_length=15, blank=True)
+    style = models.CharField(choices=STYLES, max_length=15, blank=True)
+    meter = models.PositiveSmallIntegerField(
+        choices=METERS, blank=True, null=True, default=4
+    )
+    year = models.PositiveSmallIntegerField(blank=True, null=True)
+    is_contrafact = models.BooleanField(blank=True, null=True, default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, blank=True, null=True
+    )
+
+    @property
+    def is_minor(self):
+        """
+        Is the tune in a minor key?
+        """
+        return self.key.endswith("-")
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"Tune {self.id} | {self.title}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class RepertoireTune(models.Model):
+    """
+    RepertoireTune is the ManyToMany table connecting tunes to users ("players").
+    It holds lots of essential data about a player's relationship to the tune,
+    such as when it was last played or how well they know it.
+    """
+
+    KNOWLEDGES = [
+        ("know", "know"),
+        ("learning", "learning"),
+        ("don't know", "don't know"),
+    ]
+
+    tune = models.ForeignKey(Tune, on_delete=models.CASCADE)
+    player = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    last_played = models.DateTimeField(blank=True, null=True)
+    knowledge = models.CharField(
+        choices=KNOWLEDGES, max_length=15, default="know", blank=True
+    )
+    started_learning = models.DateTimeField(blank=True, null=True)
+    play_count = models.IntegerField(default=0)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="repertoire_tunes",
+        blank=True,
+    )
+
+    class Meta:
+        unique_together = ("tune", "player")
+
+    def __str__(self):
+        return f"{self.tune} | {self.player}"
+
+
+# class Plays(models.Model):
